7ca06b12917d11e9ac4072eb46f06871ec816423
RNO <arnaud.juracek@gmail.com>
2018-02-15 19:22:16 +0100

    Remove /generator

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import handshake from 'utils/handshake'
 import Navigo from 'navigo'
 import viewer from 'pages/viewer'
 import remote from 'pages/remote'
-import generator from 'pages/generator'
 
 import LogScreen from 'components/log-screen'
 
@@ -26,8 +25,7 @@ loading.mount(document.body)
 const router = new Navigo(null, false)
 const routes = {
   '/': () => hs('viewer').then(viewer.setup),
-  '/remote': () => hs('remote').then(remote.waitForSlot),
-  '/generator': () => hs('generator').then(generator.setup)
+  '/remote': () => hs('remote').then(remote.waitForSlot)
 }
 
 Object.entries(routes).forEach(([endpoint, callback]) => {
--- src/pages.js
+++ src/pages.js
@@ -6,17 +6,9 @@ module.exports = [
     content: { title: title + ' — viewer' },
     layout: 'pages/viewer.hbs'
   },
-
   {
     output: 'remote.html',
     content: { title: title + ' — remote' },
     layout: 'pages/remote.hbs'
-  },
-
-  {
-    output: 'generator.html',
-    content: { title: title + ' — generator' },
-    layout: 'pages/generator.hbs'
   }
-
 ]
